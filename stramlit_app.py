5faba08e74d2200a70aa4012becb2761152cfc24
Laxman Singh <48987933+LakshmanRawat@users.noreply.github.com>
2022-07-13 18:26:45 +0530

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -4,7 +4,6 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('🥣Breakfast Menu')
@@ -28,18 +27,16 @@ streamlit.dataframe(fruits_to_show)
 
 #new section to display fruityvise api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+    streamlit.write('The user entered ', fruit_choice)
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response.json())
-
-# take the json version and normalize it 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output it screen as table
-streamlit.dataframe(fruityvice_normalized)
-#don't run anything past here while we troubleshoot
+except URLError as e:
 streamlit.stop()
 
 
